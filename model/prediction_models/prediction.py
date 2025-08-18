e09fc721c0d7d336f5b65feca6715dea299a3bf9
Mcfumengkai <mcfumk@outlook.com>
2025-08-18 19:47:23 +0800

    Add files via upload

---
+++ model/prediction_models/prediction.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import joblib
+
+feature_columns = [
+    'eth_balance', 'total_txs', 'sent_txs', 'received_txs',
+    'sent_to_contract_txs', 'received_from_contract_txs',
+    'external_txs','internal_txs'
+]
+
+MODEL_DIR = ''
+
+
+def predict_professional(user_features):
+    """修复特征名称警告的预测函数"""
+    clf = joblib.load('random_forest_classifier.pkl')
+    imputer = joblib.load('imputer.pkl')
+    scaler = joblib.load('scaler.pkl')
+
+
+    if isinstance(user_features, dict):
+
+        features_df = pd.DataFrame([user_features])[feature_columns]
+    elif isinstance(user_features, pd.Series):
+
+        features_df = pd.DataFrame([user_features])[feature_columns]
+    else:
+
+        features_df = pd.DataFrame([user_features], columns=feature_columns)
+
+
+    features_imputed = imputer.transform(features_df)
+    features_scaled = scaler.transform(features_imputed)
+
+
+    pro = clf.predict_proba(features_scaled)[0, 1]
+    return float(pro)
